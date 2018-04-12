535a19737d44fe8b948505dd395e5de744ee2de8
smannas98 <seanmannas02@gmail.com>
2018-04-12 15:59:12 -0400

    added delete text to my delete button

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -10,7 +10,7 @@ export class ToDo extends Component {
         <li>
           <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
           <span>{ this.props.description }</span>
-          <button  onClick={ () => this.props.deleteToDo(this.props.todoId) }  />
+          <button  onClick={ () => this.props.deleteToDo(this.props.todoId) } >Delete</button>
         </li>
         );
   }
